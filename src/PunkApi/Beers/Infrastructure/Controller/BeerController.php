1b6377117f894d47f2bf22c910dbaf643e72bbd3
Alejandro Virlan <avirlanbrg@gmail.com>
2022-12-09 06:53:31 +0100

    Added NelmioAPIBundle, modify routes(beer.yaml) and developed API documentation

--- src/PunkApi/Beers/Infrastructure/Controller/BeerController.php
+++ src/PunkApi/Beers/Infrastructure/Controller/BeerController.php
@@ -2,15 +2,15 @@
 
 namespace App\PunkApi\Beers\Infrastructure\Controller;
 
-
 use App\PunkApi\Beers\Application\GetBeerUseCase;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 class BeerController extends AbstractController
 {
     private string $base_url = 'https://api.punkapi.com/v2/beers/';
